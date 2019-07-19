c2b644c9678c4d15e3caff4f403f8b8ce41f36a9
ckhuang <ckhuang@ucsc.edu>
2019-07-18 17:45:57 -0700

    Merged all three api functions into one server.js

---
+++ Server/usps.js
@@ -0,0 +1,72 @@
+// Javascript program to call fedex APIs
+// includes input from html form
+const fetch = require('node-fetch');
+
+const PRODUCTION_URL = 'http://production.shippingapis.com/ShippingAPI.dll';
+// const TESTING_URL = `http://production.shippingapis.com/ShippingApi.dll?API=RateV4&XML=<RateV4Request USERID="098GIATR6369">
+
+// <Package ID="1ST"> 
+// <Service>PRIORITY</Service> 
+// <ZipOrigination>${body.shipper_ZipCode}</ZipOrigination> 
+// <ZipDestination>${body.recipient_ZipCode}</ZipDestination> 
+// <Pounds>${body.weight_Value}</Pounds> 
+// <Ounces>8</Ounces> 
+// <Container>NONRECTANGULAR</Container> 
+// <Size>LARGE</Size> 
+// <Width>${body.package_Width}</Width> 
+// <Length>${body.package_Length}</Length> 
+// <Height>${body.package_Height}</Height> 
+// <Girth>55</Girth> 
+// </Package> 
+// </RateV4Request> `;
+
+// keys
+const FEDEX_ACCOUNT_NUMBER = '510088000';
+const FEDEX_METER_NUMBER = '100417677';
+const FEDEX_KEY = 'HaaYYa3AMmOQGZYL';
+const FEDEX_PASSWORD = '8JihjDHJ6KSnSApCtGjtEopoa';
+const FEDEX_SERVICE_TYPE = 'FEDEX_GROUND';
+
+// function that makes an xml form to send to the API
+// it contains macros for all the variables
+
+const makeXml = (body) => 
+`http://production.shippingapis.com/ShippingApi.dll?API=RateV4&XML=<RateV4Request USERID="098GIATR6369">
+
+<Package ID="1ST"> 
+<Service>PRIORITY</Service> 
+<ZipOrigination>${body.shipper_ZipCode}</ZipOrigination> 
+<ZipDestination>${body.recipient_ZipCode}</ZipDestination> 
+<Pounds>${body.weight_Value}</Pounds> 
+<Ounces>8</Ounces> 
+<Container>NONRECTANGULAR</Container> 
+<Size>LARGE</Size> 
+<Width>${body.package_Width}</Width> 
+<Length>${body.package_Length}</Length> 
+<Height>${body.package_Height}</Height> 
+<Girth>55</Girth> 
+</Package> 
+</RateV4Request> `;
+
+// main rate function
+// calls the API and returns the rate or an error if the API returns an error
+// for incorrect input
+async function rateAsync(body) {
+    const url = makeXml(body);
+    const res = await fetch(url, { method: 'POST' }); // fetch to the API
+    const resXml = await res.text();
+    //const result = resXml.match(/<HighestSeverity>([^<]+)<\/HighestSeverity>/);
+    if (res.status === 200) { // if the API call was successful
+        const regex = /<Rate>([^<]+)<\/Rate>/;
+        const match = resXml.match(regex);
+        const price = match !== null ? match[1] : "NaN";
+        return `Price is: ${price}`;
+    } else {
+        const regex = /<Postage CLASSID="1"><MailService>Priority Mail 2-Day&amp;lt;sup&amp;gt;&amp;#8482;&amp;lt;\/sup&amp;gt;<\/MailService><Rate>([^<]+)<\/Rate><\/Postage>/;
+        const match = resXml.match(regex);
+        const error = match !== null ? match[1] : "unknown error";
+        return `Error: ${error}`;
+    }
+}
+
+module.exports.rateAsync = rateAsync;
---
+++ ups.js
@@ -0,0 +1,115 @@
+//This allows the program to run the node-fetch module
+const fetch = require('node-fetch');
+
+//API URLs given in the documentation
+const TESTING_URL = 'https://wwwcie.ups.com/ups.app/xml/Rate';
+const PRODUCTION_URL = 'https://onlinetools.ups.com/ups.app/xml/Rate';
+
+//UPS credentials
+const upsId = "ckhuang18";
+const upsPw = "Shipsterr115";
+const upsKey = "FD665C9244786FF5";
+
+//xml form that is sent to the api with macros inside for a dynamic api.
+const makeXml = (body) =>
+`<?xml version="1.0"?>
+<AccessRequest xml:lang="en-US">
+    <AccessLicenseNumber>${upsKey}</AccessLicenseNumber>
+    <UserId>${upsId}</UserId>
+    <Password>${upsPw}</Password>
+</AccessRequest>
+<?xml version="1.0"?>
+<RatingServiceSelectionRequest xml:lang="en-US">
+    <Request>
+        <TransactionReference>
+            <CustomerContext>Your Customer Context</CustomerContext>
+        </TransactionReference>
+        <RequestAction>Rate</RequestAction>
+        <RequestOption>Rate</RequestOption>
+    </Request>
+    <Shipment>
+        <Shipper>
+            <Name>Shipper Name</Name>
+            <AttentionName>Shipper Attention Name</AttentionName>
+            <PhoneNumber>1234567890</PhoneNumber>
+            <FaxNumber>1234567890</FaxNumber>
+            <ShipperNumber>Your Shipper Number</ShipperNumber>
+            <Address>
+                <AddressLine1>${body.shipper_Address}</AddressLine1>
+                <City>${body.shipper_City}</City>
+                <StateProvinceCode>${body.shipper_State}</StateProvinceCode>
+                <PostalCode>${body.shipper_ZipCode}</PostalCode>
+                <CountryCode>US</CountryCode>
+            </Address>
+        </Shipper>
+        <ShipTo>
+            <CompanyName>Ship To Company Name</CompanyName>
+            <AttentionName>Ship To Attention Name</AttentionName>
+            <PhoneNumber>1234567890</PhoneNumber>
+            <FaxNumber>1234567890</FaxNumber>
+            <Address>
+                <AddressLine1>${body.recipient_Address}</AddressLine1>
+                <City>${body.recipient_City}</City>
+                <StateProvinceCode>${body.recipient_State}</StateProvinceCode>
+                <PostalCode>${body.recipient_ZipCode}</PostalCode>
+                <CountryCode>US</CountryCode>
+            </Address>
+        </ShipTo>
+        <ShipFrom>
+            <CompanyName>Ship From Company Name</CompanyName>
+            <AttentionName>Ship From Attention Name</AttentionName>
+            <PhoneNumber>1234567890</PhoneNumber>
+            <FaxNumber>1234567890</FaxNumber>
+            <Address>
+                <AddressLine1>${body.shipper_Address}</AddressLine1>
+                <City>${body.shipper_City}</City>
+                <StateProvinceCode>${body.shipper_State}</StateProvinceCode>
+                <PostalCode>${body.shipper_ZipCode}</PostalCode>
+                <CountryCode>US</CountryCode>
+            </Address>
+        </ShipFrom>
+        <Service>
+            <Code>03</Code>
+            <Description>UPS Ground</Description>
+        </Service>
+        <Package>
+            <PackagingType>
+                <Code>02</Code>
+                <Description>UPS Package</Description>
+            </PackagingType>
+            <PackageWeight>
+                <UnitOfMeasurement>
+                    <Code>LBS</Code>
+                </UnitOfMeasurement>
+                <Weight>${body.weight_Value}</Weight>
+            </PackageWeight>
+            <Dimensions>
+                <Length>${body.package_Length}</Length>
+                <Width>${body.package_Width}</Width>
+                <Height>${body.package_Height}</Height>
+            </Dimensions> 
+        </Package>
+    </Shipment>
+</RatingServiceSelectionRequest>`;
+
+//Grabs the price we need from the response xml with regex
+async function upsRateAsync(body) {
+    const reqXml = makeXml(body);
+    const res = await fetch(TESTING_URL, { method: 'POST', body: reqXml }); // fetch to the API
+    const resXml = await res.text();
+    const result = resXml.match(/<ResponseStatusDescription>([^<]+)<\/ResponseStatusDescription>/);
+    if (res.status === 200 && result[1] === "Success") { // if the API call was successful
+        const regex = /<TotalCharges><CurrencyCode>USD<\/CurrencyCode><MonetaryValue>([^<]+)<\/MonetaryValue><\/TotalCharges>/;
+        const match = resXml.match(regex);
+        const price = match !== null ? match[1] : "NaN";
+        return `Price is: ${price}`;
+    } else {
+        const regex = /<ErrorDescription>([^<]+)<\/ErrorDescription>/;
+        const match = resXml.match(regex);
+        const error = match !== null ? match[1] : "unknown error";
+        return `Error: ${error}`;
+    }
+}
+
+//exports method to allow main file to use this function.
+module.exports.upsRateAsync = upsRateAsync;
